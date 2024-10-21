d9dd91ffaa3e6a42be61e36a48a3504d3b44f6b8
MariaClaraOliveira08 <mahh.oliveeira16@gmail.com>
2024-10-21 16:09:39 -0300

    Adicionando arquivos

--- front/app.js
+++ front/app.js
@@ -1,69 +1,95 @@
-// Acessa o objeto "document" que representa a pagina html
+//Chamada da função "createUser" para associação ao evento de envio de formulário
+document.getElementById("formulario-registro").addEventListener("submit", createUser); //metodo post
 
-//  const { json } = require("body-parser");
-//  const { error } = require("console");
-//  const { application, response } = require("express");
 
-//Seleciona o elemento com o id indicado do formulario
-document
-  .getElementById("formulario-registro")
+document.addEventListener("DOMContentLoaded", getAllUsers);
 
-  //adiciona o ouvinte de evento (submit) para capturar o envio do formulario
-  .addEventListener("submit", function (event) {
-    // previne o comportamento padrão do formulário, ou seja,
-    //continuação:- impede que ele seja enviado e recarregue a página
-    event.preventDefault();
 
-    //captura os valores dos campos dos formularios
-    const name = document.getElementById("nome").value;
-    const cpf = document.getElementById("cpf").value;
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("senha").value;
+function createUser(event) {
+  // previne o comportamento padrão do formulário, ou seja,
+  //continuação:- impede que ele seja enviado e recarregue a página
+  event.preventDefault();
 
-    //requisição HTTP para os endpoint de cadastro de usuario
-    fetch("http://localhost:5000/api/v1/user", {
-      //arrow
-      //realiza uma chamada http para o servidor (a rota definida)
-      method: "POST",
-      headers: {
-        //A requisição será em formato json
-        "Content-type": "application/json",  //Não muda
-      },
-      //Transforma os dados do formulario em uma string json para serem enviados no corpo
-      //continuação:- da requisição
-      body: JSON.stringify({ name, cpf, password, email }),
+  //captura os valores dos campos dos formularios
+  const name = document.getElementById("nome").value;
+  const cpf = document.getElementById("cpf").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("senha").value;
+
+  //requisição HTTP para os endpoint de cadastro de usuario
+  fetch("http://10.89.240.105:5000/api/v1/user/", {
+    //realiza uma chamada http para o servidor (a rota definida)
+    method: "POST",
+    headers: {
+      //A requisição será em formato json
+      "Content-type": "application/json", //Não muda
+    },
+    //Transforma os dados do formulario em uma string json para serem enviados no corpo
+    //continuação:- da requisição
+    body: JSON.stringify({ name, cpf, password, email }),
+  })
+    .then((response) => {
+      //Tratamento da resposta do servidor / API
+      if (response.ok) {
+        //Verifica se a resposta foi bem sucedida (Status 2xx)
+        return response.json(); // retorna o que a API respondeu
+      }
+      //Convertendo o erro em formato json
+      return response.json().then((err) => {
+        //Mensagem retornada do servidor, acessada pela chave "error"
+        throw new Error(err.error);
+      }); //processa e trata o erro
+    }) //Fechamento da then (response)
+    .then((data) => {
+      //Executa a resposta de sucesso retorna ao usuário final
+
+      //exibe um alerta para o usuário final (front) com o nome do usuário que acabou de ser
+      //cadastrado
+      alert(data.message);
+      console.log(data.message);
+
+      //Reset os campos do formulário após o sucesso do cadastro
+      document.getElementById("formulario-registro").reset();
     })
-      .then((response) => {
-        //Tratamento da resposta do servidor / API
-        if (response.ok) {
-          //Verifica se a resposta foi bem sucedida (Status 2xx)
-          return response.json(); // retorna o que a API respondeu
-        }
-        //Convertendo o erro em formato json
-        return response.json().then((err) => {
-          //Mensagem retornada do servidor, acessada pela chave "error"
-          throw new Error(err.error);
-        }); //processa e trata o erro
-      }) //Fechamento da then (response)
-      .then((data) => {
-        //Executa a resposta de sucesso retorna ao usuário final
+    .catch((error) => {
+      //Captura qualquer erro que ocorra durante o processo de requisição / resposta
 
-        //exibe um alerta para o usuário final (front) com o nome do usuário que acabou de ser
-        //cadastrado
-        // alert("Usuario cadastrado com sucesso! " + data.user.name);
+      //Exibe alerta (front) com o erro processado
+      alert("Erro no cadastro: " + error.message);
 
-        //Exibe o log no terminal
-        console.log("Usuario criado: ", data.user);
+      console.error("Erro:", error.message);
+    });
+}// fechamento createUser
 
-        //Reset os campos do formulário após o sucesso do cadastro
-        document.getElementById("formulario-registro").reset();
+function getAllUsers(){
+  fetch("http://10.89.240.105:5000/api/v1/user/", {
+    method: "GET", 
+    headers: {
+      "Content-Type": "application/json",
+    }
+  })
+    .then((response) => {
+      if(response.ok){ //se for igual a true esta certo
+        return response.json();
+      }
+      return response.json().then((err) => {
+        throw new Error(err.error);
+      }); //then usado para tratar respostas
+    })
+      //tratamento dos dados
+      .then((data) => { //data = dados
+        const userList = document.getElementById("user-list");
+        userList.innerHTML = "" ; //limpa a lista existente 
+
+        data.users.forEach((user)=> {
+          const listItem = document.createElement("li"); //criando um elemento em lista
+          listItem.textContent = `Nome: ${user.name}, CPF: ${user.cpf}, Email: ${user.email}` //conteudo do texto dentro dessa lista
+          userList.appendChild(listItem); // insere uma coisa dentro dela
+          //percorre o array pega o valor dele e joga pra algum lugar
+        }) 
       })
-      .catch((error) => {
-        //Captura qualquer erro que ocorra durante o processo de requisição / resposta
-
-        //Exibe alerta (front) com o erro processado
-        alert("Erro no cadastro: " + error.message);
-
-        console.error("Erro:", error.message);
-      });
-  });
+      .catch((error)=> {
+        alert("Erro ao obter usuarios" + error.mensage);
+        console.error("Erro: ", error.message);
+      })
+}
